6c8860eee003f24bde63301f679d33ed94bf3c87
manish9427 <manish119427@gmail.com>
2023-10-28 18:09:08 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import ProductPage from "./productPage/productPage";
+function App() {
+  return (
+    <div>
+      <ProductPage />
+    </div>
+  );
+}
+
+export default App;
